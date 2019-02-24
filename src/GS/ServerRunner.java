ed356d821a8b18571fc7503b973ca46cdfc7adad
Raymond Zhou <zhouc25@163.com>
2019-02-24 19:35:11 +0800

    created branch dolpin by Raymond, supposed to be Raymond's working branch

--- src/GS/ServerRunner.java
+++ src/GS/ServerRunner.java
@@ -9,6 +9,8 @@ public class ServerRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Branch - dolphin by Raymond
+		// supposed to be Raymond's working branch 
 		try {
 			ServerSocket serverSocket=new ServerSocket(8888);
 			Socket socket = null;
